0c221cf9c921de545adc2bb56799963782fd9156
Mahin12 <mahinarafatmeem@gmail.com>
2023-12-15 14:57:34 -0500

    first commit

---
+++ Components/Sidebar.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+
+interface SidebarProps{
+    children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({
+    children
+}) => {
+    const pathname = usePathname(); 
+
+    const routes = useMemo(() => [], [])
+
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
+
+export default Sidebar;
+
